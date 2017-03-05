b06acab234985e4d9513a491b23e60c5633a26e1
Felipe Brum <fbrump@gmail.com>
2017-03-05 01:30:17 -0300

    Defined the max value for number property in CreditCard

--- creditcard/models.py
+++ creditcard/models.py
@@ -4,19 +4,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 class CreditCard(models.Model):
 	"""
 		docstring for CreditCard
-		
+
 	"""
-	number = models.IntegerField(max_length=16)
+	number = models.IntegerField(validators=[MaxValueValidator(9999999999999999)])
 	date_created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        verbose_name = "CreditCard"
-        verbose_name_plural = "CreditCards"
+	class Meta:
+		verbose_name = "CreditCard"
+		verbose_name_plural = "CreditCards"
 
-    def __str__(self):
-        return '%s' % (self.number)
-    
+	def __str__(self):
+		return '%s' % (self.number)
